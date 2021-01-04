89e9ce5931144dd931f0e989cc762a8f47324ef0
SavadMKD <savadmoorkkanad@gmail.com>
2021-01-04 10:11:19 +0530

    Function For Showing Icon On Bot Click

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ select_x_btn = select_box.querySelector(".playerX");
 select_o_btn = select_box.querySelector(".playerO");
 play_board = document.querySelector(".play-board");
 all_box = document.querySelectorAll("section span");
+game_players = document.querySelector(".players");
 
 window.onload = () => { // * Once Window loaded
     for (let i = 0; i < all_box.length; i++) { // * Add onclick attribute in all available section's span
@@ -17,12 +18,23 @@ window.onload = () => { // * Once Window loaded
     select_o_btn.onclick = () => {
         select_box.classList.add("hide"); // * hide the select box when button_o clicked
         play_board.classList.add("show"); // * showing playboard section when button_o clicked
+        game_players.setAttribute("class", "players active player"); // * Adding three Classnames in player element
     }
 }
 
 let player_x_icon = "fas fa-times"; // * classname of fontawesome cross icon
-let player_o_icon = "fas fa-circle"; // * classname of fontawesome circle icon
+let player_o_icon = "far fa-circle"; // * classname of fontawesome circle icon
+
+// * User Click Function
 
 function clicked_the_box(element){
-    
+    if(game_players.classList.contains("player")){ // * if paleyers element has contains .player
+        element.innerHTML = `<i class="${player_o_icon}"></i>`; // * Adding circle icon tag inside user clicked element
+        game_players.classList.add("active");
+    }
+    else {
+        element.innerHTML = `<i class="${player_x_icon}"></i>`; // * Adding cross icon tag inside user clicked element
+        game_players.classList.add("active");
+    }
+    element.style.pointerEvents = "none"; // * once user select any box can't be selected again
 }
