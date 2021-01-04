2723d8c3ad73a7dc0bdc6683f6b05ff9e62ade0c
SavadMKD <savadmoorkkanad@gmail.com>
2021-01-04 09:35:22 +0530

    Hiding Selection box and showing on playerboard when buttonO click

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 const select_box = document.querySelector(".select-box");
 select_x_btn = select_box.querySelector(".playerX");
 select_o_btn = select_box.querySelector(".playerO");
+play_board = document.querySelector(".play-board");
 
 window.onload = () => { // Once Window loaded
-
+    select_x_btn.onclick = () => {
+        select_box.classList.add("hide");
+        play_board.classList.add("show");
+    }
 }
